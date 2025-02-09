b7186b2acbf89326d04c839c2420b3bd5a61ca2d
maciek1992 <61835495+maciek1992@users.noreply.github.com>
2025-02-09 21:29:12 +0100

    Delete dell_server_power/custom_components/dell_server_power directory

--- dell_server_power/custom_components/dell_server_power/__init__.py
+++
@@ -1,9 +0,0 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
-from .const import DOMAIN
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Uruchomienie wtyczki"""
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
-    return True
