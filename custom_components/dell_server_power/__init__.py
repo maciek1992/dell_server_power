84c171732fe9e6f032084f46d28a7cc58de3069c
maciek1992 <61835495+maciek1992@users.noreply.github.com>
2025-02-09 21:51:42 +0100

    Add files via upload

---
+++ custom_components/dell_server_power/__init__.py
@@ -0,0 +1,9 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Uruchomienie wtyczki"""
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    return True
