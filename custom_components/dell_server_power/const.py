5a00c21ed4dd7fa49ed2cd2faba40fb55335afd3
maciek1992 <61835495+maciek1992@users.noreply.github.com>
2025-02-09 22:11:09 +0100

    Add files via upload

---
+++ custom_components/dell_server_power/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "dell_server_power"
+
+CONF_IDRAC_IP = "idrac_ip"
+CONF_IDRAC_USERNAME = "idrac_username"
+CONF_IDRAC_PASSWORD = "idrac_password"
+
+CONF_MQTT_SERVER = "mqtt_server"
+CONF_MQTT_PORT = "mqtt_port"
+CONF_MQTT_USERNAME = "mqtt_username"
+CONF_MQTT_PASSWORD = "mqtt_password"
+
+DEFAULT_MQTT_PORT = 1883
+DEFAULT_ENERGY_PRICE = 0.75  # PLN/kWh
