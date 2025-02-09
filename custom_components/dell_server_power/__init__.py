102451183a32d00b70badb42ac191d578f4b0420
maciek1992 <61835495+maciek1992@users.noreply.github.com>
2025-02-09 22:56:04 +0100

    Delete custom_components/dell_server_power directory

--- custom_components/dell_server_power/__init__.py
+++
@@ -1,9 +0,0 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
-from .const import DOMAIN
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Uruchomienie wtyczki"""
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-    return True
