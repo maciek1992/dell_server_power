e2e3e5119b65650a1c8da2613bb8344690bd6be7
maciek1992 <61835495+maciek1992@users.noreply.github.com>
2025-02-10 02:13:34 +0100

    Delete custom_components/dell_server_power directory

--- custom_components/dell_server_power/__init__.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from .const import DOMAIN
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Konfiguracja wpisu integracji."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
--- custom_components/dell_server_power/const.py
+++
@@ -1,5 +0,0 @@
-DOMAIN = "dell_server_power"
-
-CONF_IDRAC_IP = "idrac_ip"
-CONF_IDRAC_USERNAME = "idrac_username"
-CONF_IDRAC_PASSWORD = "idrac_password"
